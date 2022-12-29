ab2d7528d6027e53ba3d17c71a749c9078da4967
Chertenag <chertenag@gmail.com>
2022-12-29 19:35:14 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hillel_homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

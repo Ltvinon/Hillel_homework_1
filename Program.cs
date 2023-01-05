1d92e78e999a9c72ec9ee7cd77f368b77f13aa3d
Chertenag <chertenag@gmail.com>
2023-01-05 14:24:43 +0200

    Добавлено: - вывод в консоль инфы о программе; - считывание, введенной пользователем строки; - проверка на наличие символов, не указанных в списке разрешенных; - вывод в консоль ошибки, в случае наличия таких символов.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Hillel_homework_1
+﻿using System.Text.RegularExpressions;
+
+namespace Hillel_homework_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"One line calculator.{Environment.NewLine}" +
+                $"Enter full math expresion using this allowed operation: \"+\", \"-\", \"*\", \"/\"." +
+                $"{Environment.NewLine}You can also use parentheses: \"(\", \")\" and unary minus.");
+
+            //Бесконечный цикл для повторных вычислений.
+            while (true)
+            {
+                try
+                {
+                    //Строка содержащая мат. выражение для последующего вычисления.
+                    string inputText = Console.ReadLine();
+
+                    //Удаление всех пробелов из входной строки.
+                    inputText = Regex.Replace(inputText, @"\s+", "");
+
+                    //Проверка наличия недопустимых символов во входной строке.
+                    if (new Regex(@"[^\+\-\*\/\.\(\)\s\d]").IsMatch(inputText))
+                    {
+                        throw new Exception("Invalid characters in input string");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Вывод сообщения об ошибке, полученной во время проверки правильности записи мат. выражения или его вычисления.
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine($"#################################{Environment.NewLine}Enter new expression.");
+                }
+            }
         }
     }
 }
